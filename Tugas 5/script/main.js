c73e3d0a1706eeb2532ca9592e463080c6e8e0f3
andrikuwito <andrikuwito@gmail.com>
2020-09-30 12:49:19 +0700

    Tugas

---
+++ Tugas 5/script/main.js
@@ -0,0 +1,18 @@
+function myUniversal(){
+    document.querySelector("*").style.backgroundColor="red";
+}
+function myType(){
+    document.querySelector("h1").style.backgroundColor="blue";
+}
+function myClass(){
+    document.querySelector(".container").style.backgroundColor="yellow";
+}
+function myId(){
+    document.querySelector("#logo").style.backgroundColor="red";
+}
+function myAttribute(){
+    document.querySelector("a[target]").style.backgroundColor="green";
+}
+function myGroup(){
+    document.querySelector("h2.h1").style.backgroundColor="purple";
+}
